d68b459a8dfc85f1b0b023107ec0040b0d17dde2
upsidedownmind <francisco.affonso@gmail.com>
2014-09-13 23:00:36 -0300

    menus

--- tp1/main.c
+++ tp1/main.c
@@ -6,6 +6,12 @@
 #define true  1
 #define false 0
 
+#define AGREGAR 1
+#define VER 2
+#define DESCARTAR 3
+#define SALIR 4
+
+//tipos de datos
 typedef struct STarea
 {
   int numero;
@@ -33,25 +39,43 @@ void agregar(Cola *cola, int numero, char *titulo); //push
 Tarea* tomar(Cola *cola);//pull
 Tarea* ver(Cola *cola);//peek
 
+//funciones de menu:
+int preguntarCantidadDeTareas();
+int preguntarAccion();
+void mostrarMensajeDeOpctionInvalida();
+void obtenerTareaDesdeUsuario(Cola *cola);
+void mostrarActual(Cola *cola);
+void mostrarYDescargar(Cola *cola);
+
 //codigo
 int main(int argc, char *argv[])
 {
   Cola cola;
-  int size = 10;
+  int size;
+  int opcion;
   Tarea *tarea;
+  
+  size = preguntarCantidadDeTareas();
   
   // pedir tamaño por consola
   crear(&cola, size);
   
-  //tomr datos de menu
-  agregar(&cola, 123, "test");
-  
-  //test:
-  if(estaVacia(&cola) == false) {
-    tarea = ver(&cola); //no9 l saca
-  
-    tarea = tomar(&cola); //l saca
+  while( (opcion = preguntarAccion()) != SALIR ){
+         switch(opcion) {
+             case AGREGAR:
+                 obtenerTareaDesdeUsuario(&cola);
+                 break; 
+             case VER:
+                 mostrarActual(&cola);
+                 break; 
+             case DESCARTAR:
+                 mostrarYDescargar(&cola);
+                 break; 
+             default : /* Optional */
+               mostrarMensajeDeOpctionInvalida();             
+         };
   }
+  
   
   liberar(&cola);
   
@@ -61,6 +85,51 @@ int main(int argc, char *argv[])
 }
 
 //funciones
+//////////
+// menus
+int preguntarCantidadDeTareas() {
+    return 10;//tomar del usuario
+}
+int preguntarAccion() {
+    //poner menu
+    return SALIR;
+}
+void mostrarMensajeDeOpctionInvalida(){
+     //hacer
+}
+ 
+void obtenerTareaDesdeUsuario(Cola *cola){
+  //tomr datos de menu
+  agregar(cola, 123, "test");
+  
+}
+
+void mostrarActual(Cola *cola) {
+     
+  Tarea *tarea;
+  //test:
+  if(estaVacia(cola) == false) {
+    tarea = ver(cola); //no9 l saca
+  
+  } else {
+         //mostrar error
+  }
+  
+}
+
+void mostrarYDescargar(Cola *cola){
+     
+  Tarea *tarea;
+  //test:
+  if(estaVacia(cola) == false) {  
+    tarea = tomar(cola); //l saca
+  } else {
+         //mostrar error
+  }
+}
+
+//////////
+// functiones de cola
 void crear(Cola* cola, int size){
      cola->actual = 0;
      cola->max = size;
